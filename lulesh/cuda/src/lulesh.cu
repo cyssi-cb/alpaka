144086737cf54bb3f21afefebfe63baf5aa1c712
Tim Hanel <tim.hanel@elbflorace.de>
2024-01-04 19:34:44 +0100

    helloWorld Kernel works again

--- lulesh/cuda/src/lulesh.cu
+++ lulesh/cuda/src/lulesh.cu
@@ -4679,102 +4679,103 @@ int main(int argc, char *argv[])
   // TODO: modify this constructor to account for new fields
   // TODO: setup communication buffers
   locDom = NewDomain(argv, numRanks, col, row, plane, nx, side, structured, nr, balance, cost); 
-/*
-#if USE_MPI   
-   // copy to the host for mpi transfer
-   locDom->h_nodalMass = locDom->nodalMass;
+#ifndef ALPAKA
+    #if USE_MPI   
+      // copy to the host for mpi transfer
+      locDom->h_nodalMass = locDom->nodalMass;
 
-   fieldData = &Domain::get_nodalMass;
+      fieldData = &Domain::get_nodalMass;
 
-   // Initial domain boundary communication 
-   CommRecv(*locDom, MSG_COMM_SBN, 1,
-            locDom->sizeX + 1, locDom->sizeY + 1, locDom->sizeZ + 1,
-            true, false) ;
-   CommSend(*locDom, MSG_COMM_SBN, 1, &fieldData,
-            locDom->sizeX + 1, locDom->sizeY + 1, locDom->sizeZ + 1,
-            true, false) ;
-   CommSBN(*locDom, 1, &fieldData) ;
+      // Initial domain boundary communication 
+      CommRecv(*locDom, MSG_COMM_SBN, 1,
+              locDom->sizeX + 1, locDom->sizeY + 1, locDom->sizeZ + 1,
+              true, false) ;
+      CommSend(*locDom, MSG_COMM_SBN, 1, &fieldData,
+              locDom->sizeX + 1, locDom->sizeY + 1, locDom->sizeZ + 1,
+              true, false) ;
+      CommSBN(*locDom, 1, &fieldData) ;
 
-   // copy back to the device
-   locDom->nodalMass = locDom->h_nodalMass;
+      // copy back to the device
+      locDom->nodalMass = locDom->h_nodalMass;
 
-   // End initialization
-   MPI_Barrier(MPI_COMM_WORLD);
-#endif
-
-  cudaDeviceSetCacheConfig(cudaFuncCachePreferL1);
-
-  // timestep to solution 
-  int its=0;
-
-  if (myRank == 0) {
-    if (structured)
-      printf("Running until t=%f, Problem size=%dx%dx%d\n",locDom->stoptime,nx,nx,nx);
-    else 
-      printf("Running until t=%f, Problem size=%d \n",locDom->stoptime,locDom->numElem);
-  }
-
-  cudaProfilerStart();
-
-#if USE_MPI   
-   double start = MPI_Wtime();
-#else
-   timeval start;
-   gettimeofday(&start, NULL) ;
-#endif
-
-  while(locDom->time_h < locDom->stoptime)
-  {
-    // this has been moved after computation of volume forces to hide launch latencies
-    //TimeIncrement(locDom) ;
-
-    LagrangeLeapFrog(locDom) ;
-
-    checkErrors(locDom,its,myRank);
-
-    #if LULESH_SHOW_PROGRESS
-     if (myRank == 0) 
-	 printf("cycle = %d, time = %e, dt=%e\n", its+1, double(locDom->time_h), double(locDom->deltatime_h) ) ;
+      // End initialization
+      MPI_Barrier(MPI_COMM_WORLD);
     #endif
-    its++;
-    if (its == num_iters) break;
-  }
 
-  // make sure GPU finished its work
-  cudaDeviceSynchronize();
+    cudaDeviceSetCacheConfig(cudaFuncCachePreferL1);
 
-// Use reduced max elapsed time
-   double elapsed_time;
-#if USE_MPI   
-   elapsed_time = MPI_Wtime() - start;
-#else
-   timeval end;
-   gettimeofday(&end, NULL) ;
-   elapsed_time = (double)(end.tv_sec - start.tv_sec) + ((double)(end.tv_usec - start.tv_usec))/1000000 ;
-#endif
+    // timestep to solution 
+    int its=0;
 
-   double elapsed_timeG;
-#if USE_MPI   
-   MPI_Reduce(&elapsed_time, &elapsed_timeG, 1, MPI_DOUBLE,
-              MPI_MAX, 0, MPI_COMM_WORLD);
-#else
-   elapsed_timeG = elapsed_time;
-#endif
+    if (myRank == 0) {
+      if (structured)
+        printf("Running until t=%f, Problem size=%dx%dx%d\n",locDom->stoptime,nx,nx,nx);
+      else 
+        printf("Running until t=%f, Problem size=%d \n",locDom->stoptime,locDom->numElem);
+    }
 
-  cudaProfilerStop();
+    cudaProfilerStart();
 
-  if (myRank == 0) 
-    VerifyAndWriteFinalOutput(elapsed_timeG, *locDom, its, nx, numRanks, structured);
+    #if USE_MPI   
+      double start = MPI_Wtime();
+    #else
+      timeval start;
+      gettimeofday(&start, NULL) ;
+    #endif
 
-#ifdef SAMI
-  DumpDomain(locDom) ;
-#endif
-  cudaDeviceReset();
+    while(locDom->time_h < locDom->stoptime)
+    {
+      // this has been moved after computation of volume forces to hide launch latencies
+      //TimeIncrement(locDom) ;
 
-#if USE_MPI
-   MPI_Finalize() ;
-#endif
-*/
+      LagrangeLeapFrog(locDom) ;
+
+      checkErrors(locDom,its,myRank);
+
+      #if LULESH_SHOW_PROGRESS
+        if (myRank == 0) 
+      printf("cycle = %d, time = %e, dt=%e\n", its+1, double(locDom->time_h), double(locDom->deltatime_h) ) ;
+      #endif
+      its++;
+      if (its == num_iters) break;
+    }
+
+    // make sure GPU finished its work
+    cudaDeviceSynchronize();
+
+    // Use reduced max elapsed time
+      double elapsed_time;
+    #if USE_MPI   
+      elapsed_time = MPI_Wtime() - start;
+    #else
+      timeval end;
+      gettimeofday(&end, NULL) ;
+      elapsed_time = (double)(end.tv_sec - start.tv_sec) + ((double)(end.tv_usec - start.tv_usec))/1000000 ;
+    #endif
+
+      double elapsed_timeG;
+    #if USE_MPI   
+      MPI_Reduce(&elapsed_time, &elapsed_timeG, 1, MPI_DOUBLE,
+                MPI_MAX, 0, MPI_COMM_WORLD);
+    #else
+      elapsed_timeG = elapsed_time;
+    #endif
+
+    cudaProfilerStop();
+
+    if (myRank == 0) 
+      VerifyAndWriteFinalOutput(elapsed_timeG, *locDom, its, nx, numRanks, structured);
+
+    #ifdef SAMI
+    DumpDomain(locDom) ;
+    #endif
+    cudaDeviceReset();
+
+    #if USE_MPI
+      MPI_Finalize() ;
+    #endif
+  #endif
+
   return 0 ;
 }
 
--- lulesh/cuda/src/vector.h
+++ lulesh/cuda/src/vector.h
@@ -4,11 +4,8 @@
 #include <thrust/device_vector.h>
 #include <alpaka/alpaka.hpp>
 #include <alpaka/example/ExampleDefaultAcc.hpp>
-#define ALPAKA
-using Dim1 =alpaka::DimInt<1u>;
-using Idx =std::size_t;
-using Acc = alpaka::ExampleDefaultAcc<Dim1, Idx>;
-using DevAcc = alpaka::Dev<Acc>;
+//#define ALPAKA
+
 
 template <class T>
 class Vector_h;
@@ -16,7 +13,15 @@ class Vector_h;
 template <class T>
 class Vector_d;
 
-template<typename TSource,typename Ttarget,typename Textend>
+
+
+// host vector
+#ifdef ALPAKA
+  using Dim1 =alpaka::DimInt<1u>;
+  using Idx =std::size_t;
+  using Acc = alpaka::ExampleDefaultAcc<Dim1, Idx>;
+  using DevAcc = alpaka::Dev<Acc>;
+  template<typename TSource,typename Ttarget,typename Textend>
 void alpaka_copy(TSource &source,Ttarget & target,Textend & extend){
 
   using QueueAcc = alpaka::Queue<Acc, alpaka::Blocking>;
@@ -25,9 +30,6 @@ void alpaka_copy(TSource &source,Ttarget & target,Textend & extend){
   QueueAcc queue(devAcc);
   alpaka::memcpy(queue, target,source,extend);
 };
-
-// host vector
-#ifdef ALPAKA
 template <class T>
 class Vector_h{
   Idx size_param;
@@ -103,42 +105,8 @@ class Vector_h{
  
 };
 
-#else
-template <class T>
-class Vector_h: public thrust::host_vector<T> {
-  public:
 
-  // Constructors
-  Vector_h() {}
-  inline Vector_h(int N) : thrust::host_vector<T>(N) {}
-  inline Vector_h(int N, T v) : thrust::host_vector<T>(N,v) {}
-  inline Vector_h(const Vector_h<T>& a) : thrust::host_vector<T>(a) {}
-  inline Vector_h(const Vector_d<T>& a) : thrust::host_vector<T>(a) {}
-
-  template< typename OtherVector >
-    inline void copy( const OtherVector &a ) { 
-      this->assign( a.begin( ), a.end( ) ); 
-    }
-
-  inline Vector_h<T>& operator=(const Vector_h<T> &a) { copy(a); return *this; }
-  inline Vector_h<T>& operator=(const Vector_d<T> &a) { copy(a); return *this; }
-
-  inline T* raw() { 
-    if(bytes()>0) return thrust::raw_pointer_cast(this->data()); 
-    else return 0;
-  } 
-
-  inline const T* raw() const { 
-    if(bytes()>0) return thrust::raw_pointer_cast(this->data()); 
-    else return 0;
-  } 
-
-  inline size_t bytes() const { return this->size()*sizeof(T); }
-
-};
-#endif
 // device vector
-#ifdef ALPAKA
 template <class T>
 class Vector_d{
 
@@ -217,5 +185,69 @@ class Vector_d{
         return pAcc;
       } 
 };
+#else 
+template <class T>
+class Vector_h: public thrust::host_vector<T> {
+  public:
+
+  // Constructors
+  Vector_h() {}
+  inline Vector_h(int N) : thrust::host_vector<T>(N) {}
+  inline Vector_h(int N, T v) : thrust::host_vector<T>(N,v) {}
+  inline Vector_h(const Vector_h<T>& a) : thrust::host_vector<T>(a) {}
+  inline Vector_h(const Vector_d<T>& a) : thrust::host_vector<T>(a) {}
+
+  template< typename OtherVector >
+    inline void copy( const OtherVector &a ) { 
+      this->assign( a.begin( ), a.end( ) ); 
+    }
+
+  inline Vector_h<T>& operator=(const Vector_h<T> &a) { copy(a); return *this; }
+  inline Vector_h<T>& operator=(const Vector_d<T> &a) { copy(a); return *this; }
+
+  inline T* raw() { 
+    if(bytes()>0) return thrust::raw_pointer_cast(this->data()); 
+    else return 0;
+  } 
+
+  inline const T* raw() const { 
+    if(bytes()>0) return thrust::raw_pointer_cast(this->data()); 
+    else return 0;
+  } 
+
+  inline size_t bytes() const { return this->size()*sizeof(T); }
+
+};
+template <class T>
+class Vector_d: public thrust::device_vector<T> {
+  public:
+
+  Vector_d() {}
+  inline Vector_d(int N) : thrust::device_vector<T>(N) {}
+  inline Vector_d(int N, T v) : thrust::device_vector<T>(N,v) {}
+  inline Vector_d(const Vector_d<T>& a) : thrust::device_vector<T>(a) {}
+  inline Vector_d(const Vector_h<T>& a) : thrust::device_vector<T>(a) {}
+
+  template< typename OtherVector >
+    inline void copy( const OtherVector &a ) { 
+      this->assign( a.begin( ), a.end( ) ); 
+    }
+
+  inline Vector_d<T>& operator=(const Vector_d<T> &a) { copy(a); return *this; }
+  inline Vector_d<T>& operator=(const Vector_h<T> &a) { copy(a); return *this; }
+
+  inline T* raw() { 
+    if(bytes()>0) return thrust::raw_pointer_cast(this->data()); 
+    else return 0;
+  } 
+
+  inline const T* raw() const { 
+    if(bytes()>0) return thrust::raw_pointer_cast(this->data()); 
+    else return 0;
+  } 
+  inline size_t bytes() const { return this->size()*sizeof(T); }
+};
+
 #endif
+
 
